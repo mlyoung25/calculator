dfb5efd3af8551a672e88624421bda6aa942ceaa
mlyoung25 <youmichl@umich.edu>
2023-05-11 20:39:11 -0400

    calculator sad version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="calculator-grid">
+      <div className="output">
+        <div data-previous-operand className="previous-operand"> 1234</div>
+        <div data-current-operand className="current-operand"> 123457098</div>
+      </div>
+      <button data-all-clear className="span-two">AC</button>
+      <button data-delete>DEL</button>
+      <button data-operation>÷</button>
+      <button data-number>9</button>
+      <button data-number>8</button>
+      <button data-number>7</button>
+      <button data-operation>*</button>
+      <button data-number>6</button>
+      <button data-number>5</button>
+      <button data-number>4</button>
+      <button data-operation>-</button>
+      <button data-number>3</button>
+      <button data-number>2</button>
+      <button data-number>1</button>
+      <button data-operation>+</button>
+      <button data-number>.</button>
+      <button data-number>0</button>
+      <button data-equals className="span-two">=</button>
+    </div>     
   );
 }
 
