8e16cc4783a0df430b3928706276398c9b3659a0
mlyoung25 <youmichl@umich.edu>
2023-05-16 21:09:43 -0400

    read digits

--- src/App.js
+++ src/App.js
@@ -1,33 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import DigitButton from "./DigitButton";
+import OperationButton from "./OperationButton";
+import "./App.css";
 import "./styles.css";
+import { useReducer } from "react";
+
+export const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVALUATE: "evaluate",
+};
+
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ""}${payload}`,
+      };
+  }
+}
 
 function App() {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer , {})    
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div data-previous-operand className="previous-operand"> 1234</div>
-        <div data-current-operand className="current-operand"> 123457098</div>
+        <div data-previous-operand className="previous-operand">
+          {" "}
+          {previousOperand} {operation}{" "}
+        </div>
+        <div data-current-operand className="current-operand">
+          {" "}
+          {currentOperand}{" "}
+        </div>
       </div>
-      <button data-all-clear className="span-two">AC</button>
+      <button data-all-clear className="span-two">
+        AC
+      </button>
       <button data-delete>DEL</button>
-      <button data-operation>÷</button>
-      <button data-number>9</button>
-      <button data-number>8</button>
-      <button data-number>7</button>
-      <button data-operation>*</button>
-      <button data-number>6</button>
-      <button data-number>5</button>
-      <button data-number>4</button>
-      <button data-operation>-</button>
-      <button data-number>3</button>
-      <button data-number>2</button>
-      <button data-number>1</button>
-      <button data-operation>+</button>
-      <button data-number>.</button>
-      <button data-number>0</button>
-      <button data-equals className="span-two">=</button>
-    </div>     
+      <OperationButton operation="÷" dispatch={dispatch} />
+      <DigitButton digit="9" dispatch={dispatch} />
+      <DigitButton digit="8" dispatch={dispatch} />
+      <DigitButton digit="7" dispatch={dispatch} />
+      <OperationButton operation="*" dispatch={dispatch} />
+      <DigitButton digit="6" dispatch={dispatch} />
+      <DigitButton digit="5" dispatch={dispatch} />
+      <DigitButton digit="4" dispatch={dispatch} />
+      <OperationButton operation="-" dispatch={dispatch} />
+      <DigitButton digit="3" dispatch={dispatch} />
+      <DigitButton digit="2" dispatch={dispatch} />
+      <DigitButton digit="1" dispatch={dispatch} />
+      <OperationButton operation="+" dispatch={dispatch} />
+      <DigitButton digit="." dispatch={dispatch} />
+      <DigitButton digit="0" dispatch={dispatch} />
+      <button data-equals className="span-two"> =</button>
+    </div>
   );
 }
 
---
+++ src/OperationButton.js
@@ -0,0 +1,12 @@
+import { ACTIONS } from "./App";
+
+export default function OperationButton({ operation, dispatch }) {
+  return (
+    <button
+      data-number
+      onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: operation })}
+    >
+      {operation}
+    </button>
+  );
+}
